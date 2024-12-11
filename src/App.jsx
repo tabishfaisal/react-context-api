7c24f69519286992d8c194a384d9c1808d00584a
“tabihsfaisal“ <tabish.faisaltn@gmail.com>
2024-12-11 16:16:47 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  const posts = [
+    { id: 1, title: "il primo titolo", content: "il contenuto del primo post"},
+    { id: 2, title: "il secondo titolo", content: "il contenuto del secondo post"},
+    { id: 3, title: "il terzo titolo", content: "il contenuto del terzo post"},
+    { id: 4, title: "il quarto titolo", content: "il contenuto del quarto post"},
+    { id: 5, title: "il quinto titolo", content: "il contenuto del quinto post"},
+  ]
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
